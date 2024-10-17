97f2308cda883c2d121adb3e4ff8000ee961e2e3
Casey463 <kfmconroy@gmail.com>
2024-10-17 13:37:34 -0700

    Added Types to container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 
-export default function Container({ children }) {
+export default function Container({
+  children,
+}: {
+  children: React.ReactNode | undefined;
+}) {
   return (
     <div className=" flex flex-col  max-w-7xl min-h-screen mx-auto bg-white/[2%]">
       {children}
