9e3adfc5929d0d94bdbe7ef7fc730ba44edc1113
Casey463 <kfmconroy@gmail.com>
2024-10-17 19:39:12 -0700

    Updated container typescript type format

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function Container({
-  children,
-}: {
+type ContainerProps = {
   children: React.ReactNode | undefined;
-}) {
+};
+
+export default function Container({ children }: ContainerProps) {
   return (
     <div className=" flex flex-col  max-w-7xl min-h-screen mx-auto bg-white/[2%]">
       {children}
