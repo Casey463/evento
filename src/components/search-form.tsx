220c38285d761eec8f593e333bbd222c7e5f1055
Casey463 <kfmconroy@gmail.com>
2024-10-18 16:54:10 -0700

    Made form component controlled
    Added control to form and simple directory router search

--- src/components/search-form.tsx
+++ src/components/search-form.tsx
@@ -1,17 +1,23 @@
 "use client";
 import React from "react";
+import { useRouter } from "next/navigation";
 
 export default function SearchForm() {
-  const [search, setSearch] = React.useState("");
+  const [searchText, setSearchText] = React.useState("");
+  const router = useRouter();
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    if (!searchText) return;
+
+    router.push(`/events/${searchText}`);
   };
   return (
     <div>
       <form onSubmit={(e) => handleSubmit} className="w=full sm:w-[580px]">
         <input
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
           className="w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-accent/50 transition focus:ring-2 focus:bg-white/10"
           placeholder="Search events in any city... "
           spellCheck="false"
