3013dcf393805f81bdb5ca4e063e157a410ed455
Casey463 <kfmconroy@gmail.com>
2024-10-17 19:40:28 -0700

    Updated directory icons
    Added highlights, underlines, and an animation when switching directory items.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,11 @@
+"use client";
+
 import Link from "next/link";
 import Logo from "@/components/Logo";
 import path from "path";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+import { motion } from "framer-motion";
 
 const routes = [
   {
@@ -14,20 +19,34 @@ const routes = [
 ];
 
 export default function Header() {
+  const activePathName = usePathname();
+
   return (
     <header
       className="flex items-center justify-between border-b border-white/10 h-14 px-3 sm:px-9
   "
     >
       <Logo />
-      <nav>
-        <ul className="flex gap-x-6 text-sm">
+      <nav className="h-full">
+        <ul className="flex gap-x-6 h-full text-sm">
           {routes.map((route) => (
             <li
               key={route.path}
-              className="text-white/50 hover:text-white transition"
+              className={clsx(
+                " hover:text-white flex items-center relative transition",
+                {
+                  "text-white": route.path === activePathName,
+                  "text-white/50": route.path !== activePathName,
+                }
+              )}
             >
               <Link href={route.path}>{route.name}</Link>
+              {activePathName == route.path && (
+                <motion.div
+                  layoutId="header-active-link"
+                  className="bg-accent h-1 w-full absolute bottom-0"
+                />
+              )}
             </li>
           ))}
         </ul>
