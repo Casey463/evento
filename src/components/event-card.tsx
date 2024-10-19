cae7d5bb9ea34e09244b62e1c54bee77fa2d3619
Casey463 <kfmconroy@gmail.com>
2024-10-19 14:55:37 -0700

    Added date
    Added a short date icon for each event card

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -10,7 +10,7 @@ export default function EventCard({ event }: EventCardProps) {
   return (
     <section
       className="flex flex-col flex-1 basis-80 h-[380px] w-[500px] bg-white/[3%] rounded-xl overflow-hidden
-    max-w-[500px]"
+    max-w-[500px] relative"
     >
       <Image
         src={event.imageUrl}
@@ -24,6 +24,19 @@ export default function EventCard({ event }: EventCardProps) {
         <p className="italic text-white/75">By {event.organizerName}</p>
         <p className="text-sm text-white/50 mt-4">{event.location}</p>
       </div>
+
+      <section className="absolute flex justify-center items-center flex-col left-[12px] top-[12px] h-[45px] w-[45px] bg-black/30 rounded-md">
+        <p className="text-xl font-bold -mb-[5px]">
+          {new Date(event.date).toLocaleDateString("en-US", {
+            day: "2-digit",
+          })}
+        </p>
+        <p className="text-xs uppercase text-accent">
+          {new Date(event.date).toLocaleDateString("en-US", {
+            month: "short",
+          })}
+        </p>
+      </section>
     </section>
   );
 }
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -4,7 +4,7 @@ export type EventoEvent = {
   slug: string;
   city: string;
   location: string;
-  data: Date;
+  date: Date;
   organizerName: string;
   imageUrl: string;
   description: string;
