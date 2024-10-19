bfa12f94d245ccab32d89b6696759e65660cd9f0
Casey463 <kfmconroy@gmail.com>
2024-10-19 14:36:03 -0700

    Replaced teporary item map system
    Event list component now handles mapping of events

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,4 +1,6 @@
+import EventsList from "@/components/events-list";
 import H1 from "@/components/h1";
+import { EventoEvent } from "@/lib/types";
 
 type EventPageProps = {
   params: {
@@ -12,7 +14,7 @@ export default async function EventsPage({ params }: EventPageProps) {
   const response = await fetch(
     `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
   );
-  const events = await response.json();
+  const events: EventoEvent[] = await response.json();
 
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
@@ -21,12 +23,7 @@ export default async function EventsPage({ params }: EventPageProps) {
         {city !== "All" &&
           `Events in ${city.charAt(0).toUpperCase() + params.city.slice(1)}`}
       </H1>
-
-      {events.map((event) => (
-        <section key={event.id} className="mt-8">
-          {event.name}
-        </section>
-      ))}
+      <EventsList events={events} />
     </main>
   );
 }
