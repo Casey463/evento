3da3b0c7f833374491d57445544347b6ac7c2816
Casey463 <kfmconroy@gmail.com>
2024-10-18 18:08:31 -0700

    Events page can fetch
    Events Page now fetches and displays events based on the slug

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -6,14 +6,27 @@ type EventPageProps = {
   };
 };
 
-export default function EventsPage({ params }: EventPageProps) {
-  const city = params.city.charAt(0).toUpperCase() + params.city.slice(1);
+export default async function EventsPage({ params }: EventPageProps) {
+  const city = params.city;
+
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
+  );
+  const events = await response.json();
+
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
       <H1>
-        {city === "all" && "All Events"}
-        {city !== "all" && `Events in ${city}`}
+        {city === "All" && "All Events"}
+        {city !== "All" &&
+          `Events in ${city.charAt(0).toUpperCase() + params.city.slice(1)}`}
       </H1>
+
+      {events.map((event) => (
+        <section key={event.id} className="mt-8">
+          {event.name}
+        </section>
+      ))}
     </main>
   );
 }
