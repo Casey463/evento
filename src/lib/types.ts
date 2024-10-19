bbe45cb4390180bb6d09e42321ce3865f5f4b3e1
Casey463 <kfmconroy@gmail.com>
2024-10-18 18:16:28 -0700

    Added EventoEvent Type
    Type represents fetched objects at the highest level

---
+++ src/lib/types.ts
@@ -0,0 +1,11 @@
+export type EventoEvent = {
+  id: number;
+  name: string;
+  slug: string;
+  city: string;
+  location: string;
+  data: Date;
+  organizerName: string;
+  imageUrl: string;
+  description: string;
+};
