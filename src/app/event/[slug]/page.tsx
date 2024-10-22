3f14a651e5636610b2d1602cd7c3e3ecd5cfd683
Casey463 <kfmconroy@gmail.com>
2024-10-21 20:07:47 -0700

    Used params to get the pages slug

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,3 +1,13 @@
 export default function EventPage() {
   return <div>dadads</div>;
+import Image from "next/image";
+
+type EventPageProps = {
+  params: {
+    slug: string;
+  };
+};
+
+export default async function EventPage({ params }: EventPageProps) {
+  const slug = params.slug;
 }
