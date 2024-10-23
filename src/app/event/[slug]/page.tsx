5073f782b0c396ed565d3cb73fda04405a502625
Casey463 <kfmconroy@gmail.com>
2024-10-22 18:44:56 -0700

    Added background
    Background image is stretched, blurred, and positioned to make a background for the page.

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -17,6 +17,16 @@ export default async function EventPage({ params }: EventPageProps) {
   return (
     <main>
       <section></section>
+      <section className="relative overflow-hidden flex justify-center items-center">
+        <Image
+          src={event.imageUrl}
+          className="z-0 object-cover blur-3xl"
+          alt="Event background image"
+          fill
+          quality={50}
+          sizes="(max-width:1280px) 100vw, 1280px"
+          priority
+        />
       <div></div>
     </main>
   );
