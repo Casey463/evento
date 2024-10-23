e1162c56058bbe7aeb2a0c4b93704837fa85d0b8
Casey463 <kfmconroy@gmail.com>
2024-10-22 18:47:15 -0700

    Added foreground structure
    Added foreground image, date, name, organizer, for event along with get tickets button. Styled to adapt to different sized devices.

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -27,6 +27,37 @@ export default async function EventPage({ params }: EventPageProps) {
           sizes="(max-width:1280px) 100vw, 1280px"
           priority
         />
+
+        <div className="z-1 flex relative gap-6 lg:gap-x-16 flex-col md:flex-row py-14 md:py-20">
+          <Image
+            src={event.imageUrl}
+            alt={event.name}
+            width={300}
+            height={201}
+            className="rounded-xl border-2 border-white/50 object-cover"
+          />
+
+          <div className="flex flex-col">
+            <p className=" text-white/75 ">
+              {new Date(event.date).toLocaleDateString("en-US", {
+                weekday: "long",
+                month: "long",
+                day: "numeric",
+              })}
+            </p>
+
+            <H1 className="mb-2 mt-1 whitespace-nowrap lg:text-5xl">
+              {event.name}
+            </H1>
+            <p className="whitespace-nowrap text-xl text-white/75">
+              Organized by <span className="italic">{event.organizerName}</span>
+            </p>
+            <button className="bg-white/20 text-lg capitalize mt-5 md:mt-auto  w-[300px] rounded-md border-white/10 border-2 sm:w-full py-2 hover:scale-105 active:scale-[1.02] focus:scale-105 transition">
+              Get tickets
+            </button>
+          </div>
+        </div>
+      </section>
       <div></div>
     </main>
   );
