c28342fc2d36301d8bd9138286dba0d59622bfde
Casey463 <166182888+Casey463@users.noreply.github.com>
2025-04-13 18:21:16 -0700

    Added Section/Content/Heading formatter functions

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -61,3 +61,19 @@ export default async function EventPage({ params }: EventPageProps) {
     </main>
   );
 }
+
+function Section({ children }: { children: React.ReactNode }) {
+  return <section className="mb-12">{children}</section>;
+}
+
+function SectionHeading({ children }: { children: React.ReactNode }) {
+  return <h2 className="text-2xl mb-8">{children}</h2>;
+}
+
+function SectionContent({ children }: { children: React.ReactNode }) {
+  return (
+    <p className=" max-w-4xl text-lg mx-auto leading-8 text-white/75">
+      {children}
+    </p>
+  );
+}
