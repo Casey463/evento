b8c410651265b39c2bd8418985c7fd67ac896e70
Casey463 <166182888+Casey463@users.noreply.github.com>
2025-04-14 18:32:51 -0700

    Added Dedicated function to get specific Event

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -26,3 +26,10 @@ export async function getEvents(city: string) {
   return events;
 }
 
+export async function getEvent(slug: string) {
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
+  );
+  const event: EventoEvent = await response.json();
+  return event;
+}
