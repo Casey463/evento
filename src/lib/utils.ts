7afffd030cc53d17f4b824e9ecc44aa8790ea4fb
Casey463 <166182888+Casey463@users.noreply.github.com>
2025-04-14 18:32:12 -0700

    Added a Capitalization function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,17 @@
 import React from "react";
 import { twMerge } from "tailwind-merge";
 import clsx, { ClassValue } from "clsx";
+import { EventoEvent } from "./types";
 
 export default function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(...inputs));
 }
+
+export async function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export function capitalize(string: string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
