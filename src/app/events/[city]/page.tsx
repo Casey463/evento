aa5b1b48a25eabf43dbe9afb90562ee9d5edac4b
Casey463 <166182888+Casey463@users.noreply.github.com>
2025-04-16 16:02:22 -0700

    Updated EventsList to use fetcher function

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -12,20 +12,16 @@ type EventPageProps = {
 export default async function EventsPage({ params }: EventPageProps) {
   const city = params.city;
 
-  await sleep(2000);
-  const response = await fetch(
-    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
-  );
-  const events: EventoEvent[] = await response.json();
 
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
       <H1 className="mb-28">
         {city === "all" && "All Events"}
-        {city !== "all" &&
-          `Events in ${city.charAt(0).toUpperCase() + params.city.slice(1)}`}
+        {city !== "all" && `Events in ${capitalize(city)}`}
       </H1>
-      <EventsList events={events} />
+      <Suspense fallback={<Loading />}>
+        <EventsList city={city} />
+      </Suspense>
     </main>
   );
 }
