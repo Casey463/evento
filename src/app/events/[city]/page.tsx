6f9722e47fe16d5c9710ae7683e2e363d72b892f
Casey463 <166182888+Casey463@users.noreply.github.com>
2025-04-16 16:02:36 -0700

    Added custom metadata to eventlist

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,17 +1,27 @@
 import EventsList from "@/components/events-list";
 import H1 from "@/components/h1";
+import Loading from "@/components/loading";
 import { EventoEvent } from "@/lib/types";
-import { sleep } from "@/lib/utils";
+import { capitalize, sleep } from "@/lib/utils";
+import { Metadata } from "next";
+import { Suspense } from "react";
 
-type EventPageProps = {
+type Props = {
   params: {
     city: string;
   };
 };
 
-export default async function EventsPage({ params }: EventPageProps) {
+export function generateMetadata({ params }: Props): Metadata {
   const city = params.city;
+  return {
+    title: city === "all" ? "All Events" : `Events in ${capitalize(city)}`,
+    description: `Find the best events in ${capitalize(city)}`,
+  };
+}
 
+export default async function EventsPage({ params }: Props) {
+  const city = params.city;
 
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
