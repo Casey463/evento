10c6f8e0ab90344b80313b8669ce005b2912be77
Casey463 <166182888+Casey463@users.noreply.github.com>
2025-04-21 15:09:52 -0700

    Updated getEvent to use prisma client

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -27,9 +27,13 @@ export async function getEvents(city: string) {
 }
 
 export async function getEvent(slug: string) {
-  const response = await fetch(
-    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
-  );
-  const event: EventoEvent = await response.json();
-  return event;
+  const event = await prisma.eventoEvent.findUnique({
+    where: { slug: slug },
+  });
+
+  if (!event) {
+    return notFound();
+  }
+
+  return event as EventoEvent;
 }
