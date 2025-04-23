18905f3014ae97807e727fb95c5c0f15315c6f5d
Casey463 <166182888+Casey463@users.noreply.github.com>
2025-04-23 16:50:31 -0700

    Updated types

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -12,15 +12,25 @@ type Props = {
   };
 };
 
+type EventsPageProps = Props & {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+};
+
 export function generateMetadata({ params }: Props): Metadata {
   const city = params.city;
+
   return {
     title: city === "all" ? "All Events" : `Events in ${capitalize(city)}`,
     description: `Find the best events in ${capitalize(city)}`,
   };
 }
 
-export default async function EventsPage({ params }: Props) {
+export default async function EventsPage({
+  params,
+  searchParams,
+}: EventsPageProps) {
   const city = params.city;
   const page = searchParams.page || 1;
 
@@ -31,7 +41,6 @@ export default async function EventsPage({ params }: Props) {
         {city !== "all" && `Events in ${capitalize(city)}`}
       </H1>
       <Suspense fallback={<Loading />}>
-        <EventsList city={city} />
         <EventsList city={city} page={+page} />
       </Suspense>
     </main>
