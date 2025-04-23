87f0081582e333b341185ddaf2eaccbe8e735b89
Casey463 <166182888+Casey463@users.noreply.github.com>
2025-04-23 16:49:12 -0700

    Set up pagination buttons custom component

---
+++ src/components/pagination-controls.tsx
@@ -0,0 +1,9 @@
+import { ArrowLeftIcon, ArrowRightIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
+import React from "react";
+export default function PaginationControls({
+  previousPath,
+  nextPath,
+}: PaginationControlsProps) {
+  );
+}
