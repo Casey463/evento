8f8453f45e9c83dfb76c2b92021e4bc2b07a183d
Casey463 <166182888+Casey463@users.noreply.github.com>
2025-04-23 16:49:35 -0700

    Set up pagination buttons structure and styles

--- src/components/pagination-controls.tsx
+++ src/components/pagination-controls.tsx
@@ -1,9 +1,37 @@
 import { ArrowLeftIcon, ArrowRightIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
 import React from "react";
+
+type PaginationControlsProps = {
+  previousPath: string;
+  nextPath: string;
+};
+
+const btnStyles =
+  "flex items-center gap-x-2 text-white px-5 py-3 bg-white/5 rounded-md opacity-75 hover:opacity-100 transition text-sm";
+
 export default function PaginationControls({
   previousPath,
   nextPath,
 }: PaginationControlsProps) {
+  return (
+    <section className="flex justify-between w-full">
+      {previousPath ? (
+        <Link href={previousPath} className={btnStyles}>
+          <ArrowLeftIcon />
+          Back
+        </Link>
+      ) : (
+        <div />
+      )}
+      {nextPath ? (
+        <Link href={nextPath} className={btnStyles}>
+          Next
+          <ArrowRightIcon />
+        </Link>
+      ) : (
+        <div />
+      )}
+    </section>
   );
 }
