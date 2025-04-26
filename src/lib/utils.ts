6b0544c4f171e3b5e7e21b129ffc5a0743a1a52e
Casey463 <166182888+Casey463@users.noreply.github.com>
2025-04-25 20:02:36 -0700

    Added a cache for prisma getEvent function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -32,13 +32,24 @@ export const getEvents = unstable_cache(
       skip: (page - 1) * 6,
     });
 
-  return { events, totalCount } as {
-    events: EventoEvent[];
-    totalCount: number;
-  };
-}
+    let totalCount;
+    if (city == "all") {
+      totalCount = await prisma.eventoEvent.count();
+    } else {
+      totalCount = await prisma.eventoEvent.count({
+        where: {
+          city: capitalize(city),
+        },
+      });
+    }
 
-export async function getEvent(slug: string) {
+    return { events, totalCount } as {
+      events: EventoEvent[];
+      totalCount: number;
+    };
+  }
+);
+export const getEvent = unstable_cache(async (slug: string) => {
   const event = await prisma.eventoEvent.findUnique({
     where: { slug: slug },
   });
